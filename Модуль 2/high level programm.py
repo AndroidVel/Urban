b7a4c9078c60fdf43b4f2321fa8ad8035c684d84
Андрей <andreyv006@mail.ru>
2024-05-12 19:46:31 +0200

    new version

--- Модуль 2/high level programm.py
+++ Модуль 2/high level programm.py
@@ -4,6 +4,7 @@ print(number)
 s = ''
 for i in range(1, number // 2 + 1):
     for j in range(1, 20):
-        if i + j == number:
+        summ = i + j
+        if number % summ == 0 and i != j and i < j:
             s += str(i) + str(j)
 print(int(s))
