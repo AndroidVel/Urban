fe7dc8acd4ae06dc2de9819b2571915e611ee722
Андрей <andreyv006@mail.ru>
2024-06-07 14:51:18 +0200

    practice part 1

--- nim_engine.py
+++ nim_engine.py
@@ -1,23 +1,26 @@
 from random import randint
 
-_holder = []
+MAX_BUNCHES = 5
+MAX_BUNCHES_SIZE = 20
+
+_holder = {}
 
 def put_stones():
     global _holder
-    _holder = []
-    for i in range(5):
-        _holder.append(randint(1, 20))
+    _holder = {}
+    for i in range(1, MAX_BUNCHES + 1):
+        _holder[i] = (randint(1, MAX_BUNCHES_SIZE))
 
 
 def take_from_bunche(position, quantity):
-    if 1 <= position <= len(_holder):
-        _holder[position - 1] -= quantity
+    if position in _holder:
+        _holder[position] -= quantity
 
 
 def get_bunches():
-    return _holder
+    return _holder.values()
 
 
 def is_game_over():
-    return sum(_holder) == 0
+    return sum(_holder.values()) == 0
 
