52275f6be1ad3df2a0257b81a9f9a13730772bc2
Андрей <andreyv006@mail.ru>
2024-06-07 14:57:07 +0200

    Специальные методы классов

---
+++ Модуль 5/module_5_2.py
@@ -0,0 +1,11 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(floors):
+        House.numberOfFloors = floors
+        print(House.numberOfFloors)
+
+
+House.setNewNumberOfFloors(12)
+House.setNewNumberOfFloors(5)
