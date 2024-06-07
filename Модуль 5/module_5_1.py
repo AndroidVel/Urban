ac5892f2c18a7a20d6a10dd21bff20c375f3464b
Андрей <andreyv006@mail.ru>
2024-06-07 14:16:13 +0200

    Атрибуты и методы объекта

--- Модуль 5/module_5_1.py
+++ Модуль 5/module_5_1.py
@@ -2,6 +2,7 @@ class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
     def go_to(self, new_floor):
         if new_floor < self.number_of_floors:
             for i in range(1, new_floor + 1):
