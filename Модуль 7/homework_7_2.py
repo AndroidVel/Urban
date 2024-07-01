c11f87b938cbc011acf172f6e4a26ae5c84bbf1b
Андрей <andreyv006@mail.ru>
2024-07-01 13:15:07 +0200

    Позиционирование в файле

--- Модуль 7/homework_7_2.py
+++ Модуль 7/homework_7_2.py
@@ -1,3 +1,21 @@
-file_name = 'text.txt'
-with open(file_name, mode='r') as file:
-    print(file.read())
+def custom_write(file_name, strings):
+    dict_ = {}
+    number_of_line = 0
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for line in strings:
+            number_of_line += 1
+            dict_[(number_of_line, file.tell())] = line
+            file.write(line + '\n')
+    return dict_
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
