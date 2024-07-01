8c61b49da3bf4a2652594255a0643a1e42502ee6
Андрей <andreyv006@mail.ru>
2024-07-01 12:55:54 +0200

    Сложные моменты и исключения в стеке вызовов функции

--- Модуль 8/homework_8_2.py
+++ Модуль 8/homework_8_2.py
@@ -1,29 +1,25 @@
-class InvalidTypeException(Exception):
-    def __init__(self, exception, more_info):
-        self.exception = exception
-        self.more_info = more_info
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for number in numbers:
+        try:
+            result += number
+        except TypeError:
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчёта суммы - {number}')
+    return result
 
 
-class ProcessingException(Exception):
-    pass
-
-
-def summ(a, b):
+def calculate_average(numbers):
     try:
-        if isinstance(a, int) and isinstance(b, int):
-            return a + b
-        else:
-            raise InvalidTypeException('Неверный тип данных', 'Разрешены только целые числа')
-    except InvalidTypeException as exc:
-        print(f'Произошла ошибка: {exc.exception} - {exc.more_info}')
-        raise ProcessingException()
+        return personal_sum(numbers) / len(numbers)
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+    except ZeroDivisionError:
+        return 0
 
 
-try:
-    print(summ(123, 135))
-    print(summ(123, 1.5))
-    print(summ('123', 135))
-except ProcessingException:
-    print('Что-то пошло не так')
-finally:
-    print('программа дошла до конца')
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
